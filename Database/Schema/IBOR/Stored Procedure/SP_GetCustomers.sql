32f61a9105bdc3ee2dacfa551eb3ebb0ab9d6027
DamoKrish <skrisdamo@gmail.com>
2024-10-15 16:35:07 +0530

    Added SP to source

---
+++ Database/Schema/IBOR/Stored Procedure/SP_GetCustomers.sql
@@ -0,0 +1,9 @@
+create or replace procedure SP_GetCustomers()
+returns table (Id number, Name varchar)
+language sql
+as
+declare
+  res resultset default (Select ID,first_name from ibor.customer limit 10);
+begin
+  return table(res);
+end;
