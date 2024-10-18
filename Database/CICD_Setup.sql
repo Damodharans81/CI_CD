340427a7bed21fe5b58fb204f416b10402b58004
Damodharans81 <142373779+Damodharans81@users.noreply.github.com>
2024-10-18 17:50:30 +0530

    Update CICD_Setup.sql

--- Database/CICD_Setup.sql
+++ Database/CICD_Setup.sql
@@ -35,21 +35,22 @@ CREATE OR ALTER TABLE {{dbname}}.CI_CD.CHANGE_HISTORY
 CREATE OR REPLACE SEQUENCE {{dbname}}.CI_CD.RELEASE_SEQ START = 1 INCREMENT = 1 ORDER;
 
 
-CREATE OR REPLACE PROCEDURE COMMON.RELEASE_PROC(RELEASE_TITLE VARCHAR(500))
-// Fetch the latest updates from Github
-snowflake.execute({sqlText: " ALTER GIT REPOSITORY ATOBIN_GIT_REPO FETCH;"});
-// Generate a new number from our sequence to associate with the release.
-release_no = snowflake.execute({sqlText: "select common.release_seq.nextval;"});
-release_no = release_no.getColumnValue(1);
-// Retrieve a list of files to be deployed in this release.
-deploy_files = snowflake.execute({sqlText: "Select $1 from @ATOBIN_GIT_REPO/branches/main/gitint/deploy.sql"});
-file_counter = deploy_files.getRowCount();
-// If files are listed, deploy the code and make an entry into the log table.
-result = "No files specified for release";
-for (i=0; i < file_counter; i++)
-deploy_file = deploy_files.getColumnValue(1);
-//{Placeholder for testing logic}
-snowflake.execute({sqlText: `execute immediate from @atobin_git_repo/branches/main${deploy_file}`});
-snowflake.execute({sqlText: `
-insert into COMMON.RELEASE_LOG(RELEASE_NO, RELEASE_TITLE, FILE_NAME, RELEASED_BY, RELEASE_TIMESTAMP) values
-(${release_no}, '${RELEASE_TITLE}', '${deploy_file}', current_user(), current_timestamp())`});
+
+CREATE OR REPLACE PROCEDURE {{dbname}}.CI_CD.DEPLOY(dbname VARCHAR )
+RETURNS VARCHAR 
+LANGUAGE SQL
+AS
+DECLARE
+  query VARCHAR;
+  res varchar;
+BEGIN
+  res:= 'SUCCESS';
+  // Fetch the latest updates from Github
+  query := ' ALTER GIT REPOSITORY '|| dbname ||'.PUBLIC.CI_CD_GIT_REPO FETCH ;'; 
+  EXECUTE IMMEDIATE :query ;
+
+  // Generate a new number from our sequence to associate with the release.
+  select common.release_seq.nextval;
+  
+  RETURN query;
+END;
