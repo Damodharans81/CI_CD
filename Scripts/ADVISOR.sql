d7e182815560022db598073464d4ac20927426e1
Damodharans81 <142373779+Damodharans81@users.noreply.github.com>
2024-11-07 14:01:44 +0530

    Add update script code

---
+++ Scripts/ADVISOR.sql
@@ -0,0 +1,6 @@
+create or replace TABLE {{ database }}.ABOR.ADVISOR (
+	ID NUMBER(38,0) autoincrement start 1 increment 1 noorder,
+	NAME VARCHAR(100) NOT NULL,
+	ROLE VARCHAR(100),
+	CREATED_AT TIMESTAMP_NTZ(9) DEFAULT CURRENT_TIMESTAMP()
+);
