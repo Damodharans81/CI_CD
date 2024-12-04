4f8c721ad20d2c4d39dad0a154dbf7bb72f5eda4
Damodharans81 <142373779+Damodharans81@users.noreply.github.com>
2024-12-04 17:08:35 +0530

    Add update script code

--- Scripts/DATABASE/SCHEMA/ABOR/TABLE/ADVISOR.sql
+++ Scripts/DATABASE/SCHEMA/ABOR/TABLE/ADVISOR.sql
@@ -4,6 +4,5 @@ create or replace TABLE {{ database }}.ABOR.ADVISOR (
 	ID NUMBER(38,0) autoincrement start 1 increment 1 noorder,
 	NAME VARCHAR(100) NOT NULL,
 	ROLE VARCHAR(100),
-	DEPARTMENT VARCHAR(1000),
 	CREATED_AT TIMESTAMP_NTZ(9) DEFAULT CURRENT_TIMESTAMP()
 );
