f6f6ed22b368c6d36980cc674bf72500bf66fb99
Damodharans81 <142373779+Damodharans81@users.noreply.github.com>
2024-12-05 10:53:44 +0530

    Update VW_CUSTOMER_NEW.sql

--- Scripts/DATABASE/SCHEMA/CI_CD/VIEW/VW_CUSTOMER_NEW.sql
+++ Scripts/DATABASE/SCHEMA/CI_CD/VIEW/VW_CUSTOMER_NEW.sql
@@ -1,13 +1,4 @@
 #!jinja 
 
-create or replace view {{ database }}.CI_CD.VW_CUSTOMER_NEW(
-	CUSTOMER_ID,
-	FIRST_NAME,
-	LAST_NAME,
-	PHONE_NO,
-	EMAIL,
-	STATE,
-	AGE,
-	LAST_MODIFIED_DATE
-) as
+create or replace view {{ database }}.CI_CD.VW_CUSTOMER_NEW as
 SELECT * FROM {{ database }}.CI_CD.VW_CUSTOMER;
