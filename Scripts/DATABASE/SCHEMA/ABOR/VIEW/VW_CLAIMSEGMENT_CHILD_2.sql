cf2f2e6f136046142e2cb8099c09f8110da2a650
Damodharans81 <142373779+Damodharans81@users.noreply.github.com>
2025-02-19 14:34:45 +0530

    updated

--- Scripts/DATABASE/SCHEMA/ABOR/VIEW/VW_CLAIMSEGMENT_CHILD_2.sql
+++ Scripts/DATABASE/SCHEMA/ABOR/VIEW/VW_CLAIMSEGMENT_CHILD_2.sql
@@ -2,6 +2,6 @@
 
 create or replace view {{ database }}.ABOR.VW_CLAIMSEGMENT_CHILD_2(
 	CLAIMID,
-	SEGMENTID,
-	RECORD_DATE
-) as select * from claimsegment;
+	SEGMENTID
+) as select CLAIMID,
+	SEGMENTID from claimsegment;
