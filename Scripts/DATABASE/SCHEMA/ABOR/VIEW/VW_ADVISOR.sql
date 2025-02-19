58d6abe254d3a28c916cb9aab18424c3b07df0fd
Damodharans81 <142373779+Damodharans81@users.noreply.github.com>
2025-02-19 14:32:31 +0530

    updated

--- Scripts/DATABASE/SCHEMA/ABOR/VIEW/VW_ADVISOR.sql
+++ Scripts/DATABASE/SCHEMA/ABOR/VIEW/VW_ADVISOR.sql
@@ -7,4 +7,8 @@ create or replace view {{ database }}.ABOR.VW_ADVISOR(
 	CREATED_DATE,
 	UPDATE_BY
 ) as
-select * from {{ database }}.ABOR.Advisor;
+select ID,
+	NAME,
+	ROLE,
+	CREATED_DATE,
+	UPDATE_BY from {{ database }}.ABOR.Advisor;
